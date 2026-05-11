e822c5557673df4fc1e383d8a29d37589d48c30b
dj-pmfst <djuric@pmfst.hr>
2026-05-11 13:29:53 +0200

    reformat register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-// import Login from "./pages/auth/auth";
-import Pricing from "./pages/pricing/pricing"
+import Login from "./pages/auth/auth";
+// import Pricing from "./pages/pricing/pricing"
 // import Home from './pages/Home';
 // import Storage from './pages/Storage';
 // import Scan from './pages/Scan/Scan';
@@ -10,9 +10,9 @@ import Pricing from "./pages/pricing/pricing"
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/pricing" replace />} />
-      {/* <Route path="/auth" element={<Login />} /> */}
-      <Route path="/pricing" element={<Pricing/>} />
+      <Route path="/" element={<Navigate to="/auth" replace />} />
+      <Route path="/auth" element={<Login />} />
+      {/* <Route path="/pricing" element={<Pricing/>} /> */}
       {/* <Route path="/home" element={<Home />} />
         <Route path="/storage" element={<Storage />} />
         <Route path="/scan" element={<Scan />} />
