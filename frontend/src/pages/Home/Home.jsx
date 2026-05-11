105ad128aa4ed727f1c8952eff9a330f9059606d
dj-pmfst <djuric@pmfst.hr>
2026-05-11 13:55:35 +0200

    initialize layout component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Layout from '../components/Layout';
+import styles from "./Home.module.css"
 
 export default function Home() {
   const [query, setQuery] = useState('');
@@ -7,6 +8,20 @@ export default function Home() {
 
   return (
     <Layout>
+      <div className={styles.container}>
+        <p className={styles.sectionLabel}>ADD ITEMS</p>
+
+        <div className="search-bar" style={{ marginBottom: 20 }}>
+          <input
+            type="text"
+            placeholder="Coca Cola (0.33 l)"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+          <img src="/assets/search.svg"/>
+        </div>
+
+      </div>
     </Layout>
   );
 }
