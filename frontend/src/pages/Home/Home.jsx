4a0f83f2ea07f3a53f2a4298a1596e544d301da9
dj-pmfst <djuric@pmfst.hr>
2026-05-11 20:45:34 +0200

    setup home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Layout from "../../components/Layout/Layout";
-import styles from "./Home.module.css"
+import Layout from '../components/Layout';
+import styles from './Home.module.css';
 
 export default function Home() {
   const [query, setQuery] = useState('');
-  const [itemFound, setItemFound] = useState(false);
+  const itemFound = query.toLowerCase().includes('coca');
 
   return (
     <Layout>
@@ -18,9 +18,38 @@ export default function Home() {
             value={query}
             onChange={e => setQuery(e.target.value)}
           />
-          <img src="/assets/search.svg"/>
+          <img src="/assets/search.svg" />
         </div>
 
+        {itemFound ? (
+          <>
+            <div className={styles.foundBanner}>
+              <p className={styles.foundTitle}>THE ITEM HAS<br />BEEN MADE!</p>
+              <img src="/assets/wand.svg"/>
+            </div>
+
+            <div className={`product-card ${styles.cardWrap}`}>
+              <img src="/assets/cola.png" />
+              <div className="product-info">
+                <p className="product-name">Coca Cola</p>
+                <p className="product-sub">330 ml</p>
+              </div>
+              <button className="add-btn">
+                <img src="/assets/plus.svg" />
+              </button>
+            </div>
+
+            <div className={styles.foundActions}>
+              <button className="btn-primary">ADD QUANTITY</button>
+              <button className="btn-outline">TRANSFER TO STORAGE</button>
+            </div>
+          </>
+        ) : (
+          <div className={styles.emptyState}>
+            <img src="/assets/storage.png" />
+            <p className={styles.emptyTitle}>ADD NEW ITEMS<br />TO YOUR STORAGE</p>
+          </div>
+        )}
       </div>
     </Layout>
   );
