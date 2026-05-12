3285bf28e42aed09ece3f9e18866c34c13387c9d
dj-pmfst <djuric@pmfst.hr>
2026-05-12 13:53:17 +0200

    start scan page

--- frontend/src/pages/Scan/Scan.jsx
+++ frontend/src/pages/Scan/Scan.jsx
@@ -6,6 +6,15 @@ export default function Scan() {
 
   return (
     <Layout>
+      <div className={styles.container}>
+        <p className={styles.pageTitle}>
+          {state === 'camera' ? 'SCAN' : 'SCAN FEEDBACK'}
+        </p>
+
+        {state === 'camera'   && <ScanCamera onScan={handleScan} />}
+        {state === 'loading'  && <ScanLoading />}
+        {state === 'feedback' && <ScanFeedback />}
+      </div>
     </Layout>
   );
 }
